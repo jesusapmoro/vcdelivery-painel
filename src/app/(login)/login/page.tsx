2395af86e176a008d65f2a73ec37e77819afd0e4
jesusapmoro <jesusapmoro@gmail.com>
2023-06-15 17:34:21 -0300

    Tela de Login 2

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,36 +1,67 @@
 "use client";
 
 
-import { Box, Button, TextField, Typography, Link as MuiLink } from "@mui/material";
+import { Box, Button, TextField, Typography, Link as MuiLink, Alert } from "@mui/material";
 import Link from "next/link";
+import { useState, FormEvent } from "react";
 
 const Page = () => {
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false); //verificar se esta fazendo login
+    const [emailField, setEmailField] = useState('');
+    const [passwordField, setPasswordField] = useState('');
+
+    //prevençao(parar) do evento padrão que atualizar a pagina(event: FormEvent<HTMLFormElement>)
+    //dizendo que o elemento é proprio do form
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        // !(se não timer emai ou senha)
+        if(!emailField || !passwordField) {
+            setError('Preencha e-mail e senha.');
+            return;//parar a aplicação
+        }
+
+        setError(''); //limpa os erros de tentativas anteriores
+        setLoading(true);
+        //fazer a consulta na API
+    }
+
     return(
        <>
             <Typography component="p" sx={{ textAlign: 'center', mt: 2, color: '#555'}}>Digite seus dados para entrar no paunel administrativo do estabelecimento e gerenciar produtos/pedidos.</Typography>
 
-            <Box component="form" sx={{ mt: 3 }}>
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
                 <TextField 
                     label="Digite seu e-mail" 
                     name="email"
-                    required
                     fullWidth
                     autoFocus
                     sx={{ mb : 2 }}
+                    onChange={e => setEmailField(e.target.value)}
+                    value={emailField}
+                    disabled={loading} //quando tiver carregando desabilita o campo
                 />
                 <TextField 
                     label="Digite sua senha"
                     name="password"
                     type="password"
-                    required
                     fullWidth
                     sx={{ mb: 2 }}
+                    onChange={e => setPasswordField(e.target.value)}
+                    value={passwordField}
+                    disabled={loading}
                 />
                 <Button
                     type="submit"
                     variant="contained"
                     fullWidth
-                >Entrar</Button>
+                    disabled={loading}
+                >{loading ? 'Carregado...' : 'Entrar'}</Button>
+
+                {error &&
+                <Alert variant="filled" severity="error" sx={{ mt: 3 }}>{error}</Alert>
+                }
 
                 <Box sx={{ mt: 3 }}>
                     <MuiLink href="/login/forgot" variant="body2" component={Link}>Esqueceu sua senha?</MuiLink>
