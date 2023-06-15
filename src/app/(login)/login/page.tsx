e208e1cbf3a39281b66729623fd1c9aabeaed3c4
jesusapmoro <jesusapmoro@gmail.com>
2023-06-15 16:49:32 -0300

    Tela de Login 1

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,12 +1,42 @@
 "use client";
 
-import { Box } from "@mui/material";
+
+import { Box, Button, TextField, Typography, Link as MuiLink } from "@mui/material";
+import Link from "next/link";
 
 const Page = () => {
     return(
-        <Box>
-            ....
-        </Box>
+       <>
+            <Typography component="p" sx={{ textAlign: 'center', mt: 2, color: '#555'}}>Digite seus dados para entrar no paunel administrativo do estabelecimento e gerenciar produtos/pedidos.</Typography>
+
+            <Box component="form" sx={{ mt: 3 }}>
+                <TextField 
+                    label="Digite seu e-mail" 
+                    name="email"
+                    required
+                    fullWidth
+                    autoFocus
+                    sx={{ mb : 2 }}
+                />
+                <TextField 
+                    label="Digite sua senha"
+                    name="password"
+                    type="password"
+                    required
+                    fullWidth
+                    sx={{ mb: 2 }}
+                />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    fullWidth
+                >Entrar</Button>
+
+                <Box sx={{ mt: 3 }}>
+                    <MuiLink href="/login/forgot" variant="body2" component={Link}>Esqueceu sua senha?</MuiLink>
+                </Box>
+            </Box>
+       </>
     )
 }
 
