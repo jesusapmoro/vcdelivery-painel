c8f666bf0f0e65fa8dfb53c3bb2c1d01cef17f31
jesusapmoro <jesusapmoro@gmail.com>
2023-06-17 12:02:36 -0300

    Pedido: Botom atualizar e campo pesquisa

--- src/app/(inside)/pedidos/page.tsx
+++ src/app/(inside)/pedidos/page.tsx
@@ -1,11 +1,49 @@
 "use client";
 
-import { Box } from "@mui/material"
+import { Refresh, Search } from "@mui/icons-material";
+import { Box, Button, InputAdornment, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 const Page = () => {
+    const [searchInput, setSearchInput] = useState('');
+
+    const handleSearchInput = () => {
+
+    }
+
+    const handleSearchKey = () => {
+
+    }
+
     return(
-        <Box>
-            Página de pedidos.
+        <Box sx={{ my: 3 }}>
+            
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography component="h5" variant="h5" sx={{ color: '#555', mr: 2 }}>Pedidos</Typography>
+                    <Button size="small" sx={{ justifyContent: { xs: 'flex-start', md: 'center' }}}>
+                        <Refresh />
+                        <Typography 
+                            component="div" sx={{ color: '#555', display: { xs: 'none', sm: 'block' }}}
+                        >Atualizar</Typography>
+                    </Button>
+                </Box>
+                <TextField
+                    value={searchInput}
+                    onChange={handleSearchInput}
+                    onKeyUp={handleSearchKey}//quando apertar o enter atualizar
+                    placeholder="Pesquise um pedido"
+                    variant="standard"
+                    //colocar um icone do mateiral Mui
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <Search />
+                            </InputAdornment>
+                        )
+                    }}
+                />
+            </Box>
         </Box>
     );
 }
