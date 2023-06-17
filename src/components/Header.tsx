67968a7c1cf53b0dfe0c55dbec397f8f7188b0f9
jesusapmoro <jesusapmoro@gmail.com>
2023-06-17 10:19:01 -0300

    Criando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,56 @@
+import { Menu } from "@mui/icons-material";
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export const Header = () => {
-    return <h1>bla, bla, bla</h1>
+    const router = useRouter();
+
+    const pageTitle = "Painel VcDelivery";
+
+    const handleLogout = () => {
+        router.push('/login');
+    }
+
+    const handleDrawerTogget = () => {
+
+    }
+
+    return (
+        <>
+            <AppBar component="nav" position="relative">
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        edge="start"
+                        sx={{ display: { sm: 'none' }}}
+                        onClick={handleDrawerTogget}
+                    >
+                        <Menu />
+                    </IconButton>
+                    <Typography
+                        component="div"
+                        variant="h6"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }}}
+                    >
+                        <Link href="/" style={{ color: '#FFF', textDecoration: 'none' }}>
+                            {pageTitle}
+                        </Link>
+                    </Typography>
+                    <Box sx={{ display: { xs: 'none', sm: 'block' }}}>
+                        <Link href="/pedidos" style={{ textDecoration: 'none' }}>
+                            <Button sx={{ color: '#FFF'}}>Pedidos</Button>
+                        </Link>
+                        <Link href="/produtos" style={{ textDecoration: 'none' }}>
+                            <Button sx={{ color: '#FFF'}}>Produtos</Button>
+                        </Link>
+                        <Link href="/categorias" style={{ textDecoration: 'none' }}>
+                            <Button sx={{ color: '#FFF'}}>Categorias</Button>
+                        </Link>
+                        <Button onClick={handleLogout} sx={{ color: '#FFF'}}>Sair</Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
 }
